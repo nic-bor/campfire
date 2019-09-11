9ee64ccbbe163ab4bc88bec71b2bfed5e22f3636
Nico Pakula <nico.pakula@mailbox.org>
2019-09-11 22:19:15 +0200

    Add build config for prod on Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,17 @@ use Mix.Config
 # before starting your production server.
 config :youtubesync, YoutubesyncWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "campfire-sync.herokuapp.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+# Configure your database
+config :youtubesync, Youtubesync.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -68,4 +77,6 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+
+#! COMMENTED BECAUSE IT'S CONFIGURED IN HEROKU
+# import_config "prod.secret.exs"
